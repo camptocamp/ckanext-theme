012ca28801bf395d5888ecd3ff9565491bdf9eb5
Antoine Abt <antoine@abt.im>
2019-02-19 08:26:35 +0100

    Set facets list & order

--- ckanext/pigma_theme/plugin.py
+++ ckanext/pigma_theme/plugin.py
@@ -1,13 +1,31 @@
+# coding: utf8
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
+
+from collections import OrderedDict
 
 
 class Pigma_ThemePlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IFacets, inherit=True)
 
     # IConfigurer
-
     def update_config(self, config_):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic', 'pigma_theme')
+
+    # IFacets
+    def dataset_facets(self, facets_dict, package_type):
+        # Ajouter les filtres, dans l'ordre d'affichage sur la page
+        return OrderedDict([
+            ('organization', u'Organisations'),
+            ('groups', u'Thématiques'),
+            ('datatype', u'Types'),
+            ('support', u'Supports'),
+            ('res_format', u'Formats'),
+            ('license_id', u'Licences'),
+            ('tags', u'Mots-clés'),
+            ('update_frequency', u'Fréquence de mise à jour'),
+            ('granularity', u'Granularité de la couverture territoriale'),
+            ])
