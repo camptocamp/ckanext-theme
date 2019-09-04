6cec4fe6ef0750b2ba749733b4d7ad21d216c209
Jean Pommier <jean.pommier@pi-geosolutions.fr>
2019-09-04 12:39:59 +0200

    Fix an error in root page du to objects being provided as dicts instead of lists

--- ckanext/theme/template_helpers.py
+++ ckanext/theme/template_helpers.py
@@ -25,7 +25,11 @@ def list_data_formats(package):
     # get first the formats inferred by default:
     formats = dict_list_or_dict_reduce(package['resources'], 'format')
     # then get the data-format values
-    data_formats = filter(lambda x: x['key'] == 'data-format', package.get('extras', []))
+    # The 3 following lines deal with root page where, strangely, objects that should be lists are provided as dicts
+    extras = package.get('extras', [])
+    if isinstance(extras, dict):
+        extras = extras.values()
+    data_formats = filter(lambda x: x['key'] == 'data-format', extras)
     data_formats = data_formats[0]['value'] if len(data_formats) > 0 else ''
     formats.extend(data_formats.split(','))
     # strip whitespaces around words and remove empty tags
