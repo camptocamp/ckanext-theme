fec879e7aa754bd832cc4915db73604e5986d25d
Jean Pommier <jean.pommier@pi-geosolutions.fr>
2019-10-31 18:05:08 +0100

    [fix] Mmmh, this empty after_map function was needed after all...

--- ckanext/theme/plugin.py
+++ ckanext/theme/plugin.py
@@ -38,6 +38,9 @@ class ThemePlugin(plugins.SingletonPlugin):
         map.connect('ckanext_theme_organizations_index', '/organization?q=&sort=package_count+desc', action='index')
         return map
 
+    def after_map(self, map):
+        return map
+
     # Iconfigurable
     def configure(self, main_config):
         theme_config = config.configure(main_config)
