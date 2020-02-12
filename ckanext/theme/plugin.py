560f19c0b4727f45942495322dbdfbbb825e5e01
Jean Pommier <jean.pommier@pi-geosolutions.fr>
2020-02-12 15:43:53 +0100

    [pigma-backport] Add postcode information on communes (when available)
    Original commit https://github.com/camptocamp/docker-ckan/commit/e16d574886e5aa94527fc94afc7d0dc25cc1889a
    (couldn't be cherry-picked because it was *before* theme became a
    submodule again.
    
    There might be several communes with exactly the same name.
    Until now, the select2 jquery dropdown tool was merging entries with similar
    names, so we only got the first entry, the others were never proposed.
    And there was no way to discriminate them.
    An laternative would be to use
    https://api-adresse.data.gouv.fr/search/?q=saint%20pierre%20du%20mont&lng=-0.9761&lat=49.3815
    and it context information, butthen we would still need to retrieve the
    id as per https://www.data.gouv.fr/fr/apidoc/#!/spatial/suggest_zones
    and then again to retrieve the bbox. It would add an additionnal request level.
    And t would also need to work on the select2 dropdown display to display
    multiline values...

--- ckanext/theme/plugin.py
+++ ckanext/theme/plugin.py
@@ -37,7 +37,7 @@ class ThemePlugin(plugins.SingletonPlugin):
         map.connect('ckanext_theme_organizations_index', '/organization?q=&sort=package_count+desc', action='index')
         return map
 
-    def after_map(selfself, map):
+    def after_map(self, map):
         return map
 
     # Iconfigurable
